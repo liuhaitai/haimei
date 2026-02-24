9c6b65d07a311bec551b17b73f189fc186632107
haitaoliu <haitaoliu@didiglobal.com>
2026-02-24 15:24:28 +0800

    不使用compose

--- app/src/main/java/com/haitao/haimei/MainActivity.kt
+++ app/src/main/java/com/haitao/haimei/MainActivity.kt
@@ -1,47 +1,15 @@
 package com.haitao.haimei
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.haitao.haimei.ui.theme.HaimeiTheme
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            HaimeiTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_main)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HaimeiTheme {
-        Greeting("Android")
+        val greetingText = findViewById<TextView>(R.id.greeting_text)
+        greetingText.text = getString(R.string.hello_android)
     }
 }
